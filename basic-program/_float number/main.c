b39fe957fc4f20a0f5416047bed848cb1f1a10e2
Shifna21 <shifnamc556@gmail.com>
2024-08-20 11:39:41 +0530

    first commit

---
+++ basic-program/_float number/main.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float a,b,c;
+    printf("enter the number:");
+    scanf("%f%f",&a,&b);
+    c=a*b;
+    printf("enter the result :%f",c);
+    return 0;
+}
